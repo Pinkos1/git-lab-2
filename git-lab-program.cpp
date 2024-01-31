4e5d4b8f505f630e4bd4a2d9837e40c7474919a4
Adam <Pinkos2003@gmail.com>
2024-01-31 18:43:23 -0500

    hi

---
+++ git-lab-program.cpp
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Adam Pinkos
+ *        Date: 1/31/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
